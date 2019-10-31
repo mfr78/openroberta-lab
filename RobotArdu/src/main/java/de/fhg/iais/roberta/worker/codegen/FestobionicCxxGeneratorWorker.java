f2c0d23c0cbbcd3f092eb35740f8dd67937f02e6
Patrick Werner <boon.werner@gmail.com>
2019-10-31 17:13:49 +0100

    add bean interface which can be accessed by subclasses via class getter

--- RobotArdu/src/main/java/de/fhg/iais/roberta/worker/codegen/FestobionicCxxGeneratorWorker.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/worker/codegen/FestobionicCxxGeneratorWorker.java
@@ -1,16 +1,14 @@
 package de.fhg.iais.roberta.worker.codegen;
 
-import de.fhg.iais.roberta.bean.CodeGeneratorSetupBean;
-import de.fhg.iais.roberta.bean.UsedHardwareBean;
+import de.fhg.iais.roberta.bean.OraBean;
 import de.fhg.iais.roberta.components.Project;
 import de.fhg.iais.roberta.visitor.codegen.FestobionicCppVisitor;
 import de.fhg.iais.roberta.visitor.lang.codegen.AbstractLanguageVisitor;
 import de.fhg.iais.roberta.worker.AbstractLanguageGeneratorWorker;
 
 public class FestobionicCxxGeneratorWorker extends AbstractLanguageGeneratorWorker {
-
     @Override
-    protected AbstractLanguageVisitor getVisitor(UsedHardwareBean usedHardwareBean, CodeGeneratorSetupBean codeGeneratorSetupBean, Project project) {
-        return new FestobionicCppVisitor(usedHardwareBean, codeGeneratorSetupBean, project.getConfigurationAst(), project.getProgramAst().getTree());
+    protected AbstractLanguageVisitor getVisitor(Project project, OraBean... beans) {
+        return new FestobionicCppVisitor(project.getProgramAst().getTree(), project.getConfigurationAst(), beans);
     }
 }
